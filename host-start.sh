0c2577f5b10f35d6e9e269f0397e96115520c551
Jon Hinks <projects.ws@jondh.me.uk>
2017-06-12 14:27:20 +0100

    Add detach and restart policies

--- host-start.sh
+++ host-start.sh
@@ -6,13 +6,16 @@
 #
 # @todo Pass an env file path to this script as a parameter
 # @todo If there is no parameter passed, throw a fatal error
+# @todo Pass host-side port in a shell parameter?
 
 export DOCKER_HOSTIP=`ifconfig docker0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
 
 echo "Connecting to database on Docker host ${DOCKER_HOSTIP}"
 
 docker run \
-	-p 127.0.0.1:8082:80 \
-	--add-host=docker:${DOCKER_HOSTIP} \
-	--env-file=config/envs/local \
-	piwik
+    --publish 127.0.0.1:8082:80 \
+    --add-host=docker:${DOCKER_HOSTIP} \
+    --env-file=config/envs/local \
+    --detach \
+    --restart always \
+    piwik
